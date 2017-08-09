396658e285309051f0dc206ac94b20ba34c55c1c
harrydry <harrydry1996@gmail.com>
2017-08-09 18:18:44 +0300

    Relates #26

--- public/index.js
+++ public/index.js
@@ -1,3 +1,17 @@
+var libraryContainer = document.querySelector('#library-container');
+
+function createAndSetAttribute (typeOfElement, className) {
+  var createdElement = document.createElement(typeOfElement);
+  createdElement.setAttribute('class', className);
+  return createdElement;
+}
+
+function createAndSetInnerText (typeOfElement, innerText) {
+  var createdElement = document.createElement(typeOfElement);
+  createdElement.innerText = innerText;
+  return createdElement;
+}
+
 function fetchData(url, cb) {
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
@@ -11,41 +25,44 @@ function fetchData(url, cb) {
   xhr.send();
 }
 
+
+      // var bookContainer = document.createElement('div');
+      // bookContainer.setAttribute('class', 'book_container')
+
 function renderBookImage(err, data) {
   if (err) {
     console.log(err);
   } else {
     var books = JSON.parse(data);
     books.forEach(function(book) {
-      var bookContainer = document.createElement('div');
-      bookContainer.setAttribute('class', 'book_container')
-      var libraryContainer = document.querySelector('#library-container'); // connect to html
-      var bookImageUrl = book.cover_url; //get url from book object
-      // var bookId = book.id;
-      var bookImg = document.createElement('img');
-      bookImg.setAttribute('class', 'book_cover');
-      // bookImg.setAttribute('id', 'bookId' + bookId);
-      bookImg.src = bookImageUrl;
+      
+      // Create and Set Classes for BookContainer / bookImg / bookModal 
+      var bookContainer = createAndSetAttribute('div', 'book_container');
+      var bookModal = createAndSetAttribute('div', 'book_modal book_modal_hide');
+      var bookImg = createAndSetAttribute('img', 'book_cover book_cover_hide');
+      
+      // Set img src to be equal to the cover_url
+      bookImg.src = book.cover_url;
+      
+      // Append BookImg and bookModal to BookContainer
       bookContainer.appendChild(bookImg);
-      var bookModal = document.createElement('div');
-      bookModal.setAttribute('class', 'book_modal');
       bookContainer.appendChild(bookModal);
+
+      // Append bookContainer to libraryContainer
       libraryContainer.appendChild(bookContainer)
-      console.log(bookImg);
-      bookImg.classList.add('book_cover_hide');
-      bookModal.classList.add('book_modal_hide');
-      var bookTitleData = book.book_name;
-      var bookAuthorData = book.author;
-      var bookTitleElement = document.createElement('p');
-      var bookAuthorElement = document.createElement('p');
-      bookTitleElement.innerText = 'Title: ' + bookTitleData;
-      bookAuthorElement.innerText = 'Author: ' + bookAuthorData;
+
+      // Create and Dynamically set Inner Text of Element
+      var bookTitleElement = createAndSetInnerText('p', 'Title: ' + book.book_name);
+      var bookAuthorElement = createAndSetInnerText('p', 'Author: ' + book.author);
+
+      // Append Created Elements to BookModal
       bookModal.appendChild(bookTitleElement);
       bookModal.appendChild(bookAuthorElement);
+
+      // Toggle Classes on Click
       bookContainer.addEventListener('click', function() {
-        bookImg.classList.toggle('book_cover_hide');
-        bookModal.classList.toggle('book_modal_hide');
-        // create a modal window
+      bookImg.classList.toggle('book_cover_hide');
+      bookModal.classList.toggle('book_modal_hide');
       })
     })
   }
